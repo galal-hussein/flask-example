f2e6d79501520dda5eea380a4a5aa5b5912a542d
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2015-10-31 17:14:44 +0200

    Add configuration to heroku

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class BaseConfig(object):
     DEBUG = False
     # Secret key has to be randomly generated
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = '\x9a\x80\xce\xa5$[6\xe0\xf2\x01\x1eD{:N"\xdc0$\xc2\xce,/\xf5'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
